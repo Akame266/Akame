b9fbe765a4b806f073ae9474eedae1afbcda8997
Akame <Datkovsky26@mail.ru>
2021-06-20 22:42:07 +0300

    rework

--- DZ14.py
+++ DZ14.py
@@ -1,11 +1,11 @@
 y = int(input('Введите желаемую высоту треугольника в цифре: '))
-y += 1
-x = y * 2 -1
+x = y * 2
 print('вот оно')
 for y1 in range(y):
     print()
     for x1 in range(x):
-        if y1 == y-1 or x1 == y1+y-1 or x1 == y-y1-1 or x1 < y1+y and not x-y-y1 > x1:
+        if y1 == y or x1 == y1+y-1 or x1 == y-y1-1 or x1 < y1+y and not x-y-y1 > x1:
             print('* ', end='')
         else:
             print('  ', end='')
+print()

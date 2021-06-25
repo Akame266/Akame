b7534ace9fb57f63e6ac6a300a1dd6dbf35231ec
Akame <Datkovsky26@mail.ru>
2021-06-25 23:45:42 +0300

    Словарь1

---
+++ DZ17.py
@@ -0,0 +1,21 @@
+s = 'Я иду туда, куда, не знаю, но иду, почему, не знаю, но иду, когда прийду, когда не знаю, тогда прийду.'
+print(s)
+s1 = s.replace(',', '')
+s1 = s1.replace('.', '')
+s1 = s1.lower()
+s2 = tuple(s1.split())
+k = 0
+f = []
+while True:
+    n = s2[k]
+    w = s2.count(str(n))
+    k += 1
+    r = (n, w)
+    f.append(r)
+    if k == len(s2):
+        break
+print(f)
+d1 = dict(f)
+print('Слова и их кол-во повторений в строке:', d1)
+
+

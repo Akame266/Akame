9c347a22a95e08d579c68d6f4a7095dfccd27f60
Akame <Datkovsky26@mail.ru>
2021-07-14 21:46:46 +0300

    Перевернутый словарь 2.0

--- DZ25.py
+++ DZ25.py
@@ -17,11 +17,11 @@ u = dict(l)
 for t,t1 in f.items():
     for e in t:
         if e in u.keys():
-            h1 = e, (t1,u[e])
-            l.append(h1)
-            u = dict(l)
+            u[e].append(t1)
         else:
-            h1 = e,t1
+            h2 = [t1]
+            h1 = e,h2
             l.append(h1)
             u = dict(l)
 print('Перевернутый словарь будет выглядеть следующим образом:',u)
+

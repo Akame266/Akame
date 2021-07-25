4263a07ea8a954c6c2f73622582eb55a8b5cc6b8
Akame <Datkovsky26@mail.ru>
2021-07-25 21:09:37 +0300

    Матрица с суммами стобцов и строчек

--- Matrix1.py
+++ Matrix1.py
@@ -1,4 +1,5 @@
 from random import randint
+
 m = int(input('Введите количество колонок: '))  # колонки
 n = int(input('Введите количество строк: '))  # строки
 a = [[randint(0, 100) for _ in range(m)] for _ in range(n)]
@@ -11,11 +12,10 @@ for k in range(n):
         s += a[k][b]
         print(o.format(y=a[k][b]), end='')
         a1[b] += a[k][b]
-    print ('\t',s)
+    print('\t', s)
 print()
 for h in a1:
-    print(o.format(y=h),end='')
-
+    print(o.format(y=h), end='')
 
 # Если без доп переменной o, которая для формата, в условии в атачче запрещенно более 1 переменной
 # for k in range(n):

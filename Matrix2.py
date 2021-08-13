ff23190f471d6e569720e09c701af1668a5c64bb
Akame <Datkovsky26@mail.ru>
2021-08-13 19:04:10 +0300

    Матрица2

--- Matrix2.py
+++ Matrix2.py
@@ -4,7 +4,6 @@ m = int(input('Введите количество колонок и строч�
 a = [[randint(1, 50) for _ in range(m)] for _ in range(m)]
 o = '{y:<8}'
 a1 = [0] * m
-print(a)
 print()
 
 for k in range(m):
@@ -14,32 +13,43 @@ for k in range(m):
     print()
 print()
 
+for h in a1:
+    print(o.format(y=h), end='')
+print()
+
 
 def bubble(array):
     for i in range(len(array) - 1):
-        flag = 1
+        kap = 1
         for j in range(len(array) - 1 - i):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
                 for s in range(len(array)):
                     a[s][j], a[s][j + 1] = a[s][j + 1], a[s][j]
-            flag = 0
-        if flag:
-            for q in range(len(array)-1):
-                for q1 in range(len(array) - 1 - q):
-                    for s in range(len(array)):
-                        a[s][j], a[s][j + 1] = a[s][j + 1], a[s][j]
-
+            kap = 0
+        if kap:
             break
 
 
-for h in a1:
-    print(o.format(y=h), end='')
-print()
-
 bubble(a1)
+
+for i1 in range(len(a)):
+    for i in range(len(a)):
+        flag = 1
+        for j in range(len(a) - 1):
+            if (i1 + 10) % 2:
+                if a[j][i1] > a[j + 1][i1]:
+                    a[j][i1], a[j + 1][i1] = a[j + 1][i1], a[j][i1]
+                flag = 0
+            else:
+                if a[j][i1] < a[j + 1][i1]:
+                    a[j][i1], a[j + 1][i1] = a[j + 1][i1], a[j][i1]
+                flag = 0
+    if flag:
+        break
+
+
 print()
-print(a)
 print()
 for k in range(m):
     for b in range(m):
@@ -49,3 +59,7 @@ print()
 for h in a1:
     print(o.format(y=h), end='')
 print()
+
+
+print()
+print('Помилуйте',chr(0x1F480))

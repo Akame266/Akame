247da3defc43ea965a8d49b2560d75feb232faa7
Akame <Datkovsky26@mail.ru>
2021-08-17 19:44:10 +0300

    Матрица реворк

--- Matrix2.py
+++ Matrix2.py
@@ -2,58 +2,55 @@ from random import randint
 
 m = int(input('Введите количество колонок и строчек: '))
 a = [[randint(1, 50) for _ in range(m)] for _ in range(m)]
-o = '{y:<8}'
-a1 = [0] * m
-print()
-
-for k in range(m):
-    for b in range(m):
-        print(o.format(y=a[k][b]), end='')
-        a1[b] += a[k][b]
-    print()
-print()
-
-for h in a1:
-    print(o.format(y=h), end='')
-print()
 
 
 def bubble(array):
-    for i in range(len(array) - 1):
+    o = '{y:<8}'
+    a1 = [0] * len(array)
+    for k in range(len(array)):
+        for b in range(len(array)):
+            print(o.format(y=array[k][b]), end='')
+            a1[b] += a[k][b]
+        print()
+    print()
+
+    for h in a1:
+        print(o.format(y=h), end='')
+    print()
+    for i in range(len(a1) - 1):
         kap = 1
-        for j in range(len(array) - 1 - i):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
+        for j in range(len(a1) - 1 - i):
+            if a1[j] > a1[j + 1]:
+                a1[j], a1[j + 1] = a1[j + 1], a1[j]
                 for s in range(len(array)):
-                    a[s][j], a[s][j + 1] = a[s][j + 1], a[s][j]
+                    array[s][j], array[s][j + 1] = array[s][j + 1], array[s][j]
             kap = 0
         if kap:
             break
-    for i1 in range(len(a)):
-        for i in range(len(a)):
+    for i1 in range(len(array)):
+        for i in range(len(array)):
             flag = 1
-            for j in range(len(a) - 1):
+            for j in range(len(array) - 1):
                 if (i1 + 10) % 2:
-                    if a[j][i1] > a[j + 1][i1]:
-                        a[j][i1], a[j + 1][i1] = a[j + 1][i1], a[j][i1]
+                    if array[j][i1] > array[j + 1][i1]:
+                        array[j][i1], array[j + 1][i1] = array[j + 1][i1], array[j][i1]
                     flag = 0
                 else:
-                    if a[j][i1] < a[j + 1][i1]:
-                        a[j][i1], a[j + 1][i1] = a[j + 1][i1], a[j][i1]
+                    if array[j][i1] < array[j + 1][i1]:
+                        array[j][i1], array[j + 1][i1] = array[j + 1][i1], array[j][i1]
                     flag = 0
         if flag:
             break
-
-
-bubble(a1)
-
-print()
-print()
-for k in range(m):
-    for b in range(m):
-        print(o.format(y=a[k][b]), end='')
     print()
-print()
-for h in a1:
-    print(o.format(y=h), end='')
-print()
+    print()
+    for k in range(len(array)):
+        for b in range(len(array)):
+            print(o.format(y=array[k][b]), end='')
+        print()
+    print()
+    for h in a1:
+        print(o.format(y=h), end='')
+    print()
+
+
+bubble(a)
--- nums.py
+++ nums.py
@@ -1,21 +1,23 @@
 from string import ascii_uppercase
-print(ascii_uppercase)
-def numrev(x,y=2):
-    a=[]
+
+a2 = ascii_uppercase.lower()
+print(a2)
+
+
+def numrev(x, y=2):
+    a = []
     b = ''
-    while x > 0:
-        a1 = x%y
-        a.append(a1)
-        x = x//y
-        print(a)
-    a.reverse()
+    if y == 2:
+        while x > 0:
+            a1 = x % y
+            a.append(a1)
+            x = x // y
+        a.reverse()
+        b1 = '0' + str(a2[-3])
     for o in a:
         b += str(o)
-    return print(a,b)
+    b2 = b1 + b
+    return print(b2)
 
 
-print(hex(1000))
-print(hex(966))
-print(hex(999))
-print(hex(1))
-print(hex(28))
+numrev(226)
